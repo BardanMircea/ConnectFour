5cf34c1122c67ac37d4f77cd2e7681b4783738af
mircea bardan <bardan.mircea.calin@gmail.com>
2023-05-05 21:39:50 +0200

    refactored game.js

--- game.js
+++ game.js
@@ -3,48 +3,18 @@ const prompt = require("prompt-sync")({ sigint: true });
 
 const gameLoop = () => {
   // create an empty board
-  let board = [
-    ["-", "-", "-", "-", "-", "-", "-"],
-    ["-", "-", "-", "-", "-", "-", "-"],
-    ["-", "-", "-", "-", "-", "-", "-"],
-    ["-", "-", "-", "-", "-", "-", "-"],
-    ["-", "-", "-", "-", "-", "-", "-"],
-    ["-", "-", "-", "-", "-", "-", "-"],
-  ];
-
-  // start game
-  console.log("Game started");
-  printBoard(board);
+  let board = showInitialBoard();
 
   // ask the players for the piece that goes first
-  const redOrYellow = ["R", "Y"];
-  let piece = prompt("Enter first move (R / Y): ").toUpperCase();
-
-  // check if piece is valid (only 'R' or 'Y' allowed)
-  while (!redOrYellow.includes(piece)) {
-    piece = prompt(
-      `Invalid move. Try again. Enter next move (R / Y)?`
-    ).toUpperCase();
-  }
+  let piece = selectStartingPiece();
 
   // if starting piece is validated, begin the game loop
   while (true) {
     // ask the current player for his move
-    let position = prompt(`Next move is ${piece}. In which column does it go?`);
+    askPlayerForMove(piece, board);
 
-    // check if the move is possible
-    let isPiecePut = putPiece(board, piece, position);
-
-    // if move impossible, ask for a valid move
-    while (
-      position > board[0].length - 1 ||
-      isNaN(position) ||
-      position < 0 ||
-      !isPiecePut
-    ) {
-      position = prompt(`Invalid move. Try again. In which column does it go?`);
-      isPiecePut = putPiece(board, piece, position);
-    }
+    // get the turn outcome
+    const outcome = checkBoard(board);
 
     // check the turn outcome
     piece = checkTurnOutcome(outcome, board, piece);
@@ -56,6 +26,55 @@ const gameLoop = () => {
 };
 
 gameLoop();
+
+// function to ask for, and validate, the next move
+function askPlayerForMove(piece, board) {
+  let position = prompt(`Next move is ${piece}. In which column does it go?`);
+
+  // check if the move is possible
+  let isPiecePut = putPiece(board, piece, position);
+
+  // if move impossible, ask for a valid move
+  while (
+    position > board[0].length - 1 ||
+    isNaN(position) ||
+    position < 0 ||
+    !isPiecePut
+  ) {
+    position = prompt(`Invalid move. Try again. In which column does it go?`);
+    isPiecePut = putPiece(board, piece, position);
+  }
+}
+
+// function to select the starting piece
+function selectStartingPiece() {
+  const redOrYellow = ["R", "Y"];
+  let piece = prompt("Enter first move (R / Y): ").toUpperCase();
+
+  // check if piece is valid (only 'R' or 'Y' allowed)
+  while (!redOrYellow.includes(piece)) {
+    piece = prompt(
+      `Invalid move. Try again. Enter next move (R / Y)?`
+    ).toUpperCase();
+  }
+  return piece;
+}
+
+// function to display initial board and start the game
+function showInitialBoard() {
+  let board = [
+    ["-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "-", "-"],
+  ];
+
+  console.log("Game started");
+  printBoard(board);
+  return board;
+}
 
 //function to check if game continues or ends
 function checkTurnOutcome(outcome, board, piece) {
